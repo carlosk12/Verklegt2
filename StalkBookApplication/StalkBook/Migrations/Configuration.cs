1a00820d5e07f15482d39ac29aac5b8ae45cc9d6
Tómas Dan Jónsson <tomasdan.jonsson@gmail.com>
2015-05-06 11:37:01 +0000

    Revert "Revert "Breytti Account Controller""
    This reverts commit d51296a746232d51083301acd09e7783206f6f7b.

---
+++ StalkBookApplication/StalkBook/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace StalkBook.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<StalkBook.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "StalkBook.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(StalkBook.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
