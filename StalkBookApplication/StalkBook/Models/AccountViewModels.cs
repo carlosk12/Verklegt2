1759f50184e92bb22a197e32cffebeddee31ce60
Tómas Dan Jónsson <tomasdan.jonsson@gmail.com>
2015-05-06 10:48:13 +0000

    Bætti við email fyrir notendur

--- StalkBookApplication/StalkBook/Models/AccountViewModels.cs
+++ StalkBookApplication/StalkBook/Models/AccountViewModels.cs
@@ -50,6 +50,12 @@ namespace StalkBook.Models
         public string UserName { get; set; }
 
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be of the format someone@example.com")]
+        [Display(Name = "Email")]
+
+        public string Email { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
