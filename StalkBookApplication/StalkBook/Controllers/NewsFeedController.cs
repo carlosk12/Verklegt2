2ac5a637a75f34154608962c06460854fbcdaefc
Carlos RAgnar <carlosk12@ru.is>
2015-05-07 11:56:02 +0000

    NewsFeed Controller added

---
+++ StalkBookApplication/StalkBook/Controllers/NewsFeedController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StalkBook.Controllers
+{
+	[Authorize]
+    public class NewsFeedController : Controller
+    {
+        // GET: NewsFeed
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
