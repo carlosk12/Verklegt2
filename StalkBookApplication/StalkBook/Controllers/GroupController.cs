7cd7b0a5d53816dc1f954227d2496bf366a2889e
Tómas Dan Jónsson <tomasdan.jonsson@gmail.com>
2015-05-08 12:22:51 +0000

    Group Virkni

--- StalkBookApplication/StalkBook/Controllers/GroupController.cs
+++ StalkBookApplication/StalkBook/Controllers/GroupController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using StalkBook.Models;
 
 namespace StalkBook.Controllers
 {
+    [Authorize]
     public class GroupController : Controller
     {
         // GET: Group
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Group group)
+        {
+            var db = new ApplicationDbContext();
+            group.name = group.name;
+            group.timeCreated = DateTime.Now;
+            db.groups.Add(group);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
