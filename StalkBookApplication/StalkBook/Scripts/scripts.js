c4e4175539ba7dc1f31165a495880b59eb7c7ab4
Ingimar Örn Oddsson <ingimar9@gmail.com>
2015-05-11 20:06:39 +0000

    up/downvote finally working

--- StalkBookApplication/StalkBook/Scripts/scripts.js
+++ StalkBookApplication/StalkBook/Scripts/scripts.js
@@ -32,17 +32,13 @@
 
     $(".rating-button").click(function (event)
     {
-        alert("im here1");
         var direction = "up";
+        var currRating = $(this).text();
         if($(this).hasClass("neutral-down") || $(this).hasClass("downvoted"))
         {
             direction = "down";
         }
-        alert("ok");
-        alert($(this).text());
-        alert(direction);
-        alert($(this).closest(".content").data('id'));
-            $.post('/NewsFeed/RateStatus/', { currRating: $(this).text(), arrowDirection: direction, statusId: $(this).closest(".content").data('id') },
+            $.post('/NewsFeed/RateStatus/', { currRating: currRating, arrowDirection: direction, statusId: $(this).closest(".content").data('id') },
             function () 
             {
 
@@ -50,7 +46,44 @@
             .fail(function () {
                 alert("Sorry there was a problem with rating this status.");
             });
-            /*$(this).text("Stalking").addClass('btn-primary').removeClass('btn-default');*/
+            var statusRating = $(this).siblings(".status-rating").first().text();
+            statusRating = parseInt(statusRating, 0);
+            if (direction == "up" && currRating == 0)
+            {
+                $(this).text("1").addClass("upvoted").removeClass("neutral-up");
+                $(this).next().text("1");
+                $(this).siblings(".status-rating").first().text(++statusRating);
+            }
+            else if (direction == "up" && currRating == 1)
+            {
+                $(this).text("0").addClass("neutral-up").removeClass("upvoted");
+                $(this).next().text("0");
+                $(this).siblings(".status-rating").first().text(--statusRating);
+            }
+            else if (direction == "up" && currRating == 2)
+            {
+                $(this).text("1").addClass("upvoted").removeClass("neutral-up");
+                $(this).next().text("1").addClass("neutral-down").removeClass("downvoted");
+                $(this).siblings(".status-rating").first().text(statusRating + 2);
+            }
+            else if (direction == "down" && currRating == 0)
+            {
+                $(this).text("2").addClass("downvoted").removeClass("neutral-down");
+                $(this).prev().text("2");
+                $(this).siblings(".status-rating").first().text(--statusRating);
+            }
+            else if (direction == "down" && currRating == 1)
+            {
+                $(this).text("2").addClass("downvoted").removeClass("neutral-down");
+                $(this).prev().text("2").addClass("neutral-up").removeClass("upvoted");
+                $(this).siblings(".status-rating").first().text(statusRating - 2);
+            }
+            else
+            {
+                $(this).text("0").addClass("neutral-down").removeClass("downvoted");
+                $(this).prev().text("0");
+                $(this).siblings(".status-rating").first().text(++statusRating);
+            }
     });
 
 });
