1662773960139d02215cc7f3f3c9cc1cade514eb
Ingimar Örn Oddsson <ingimar9@gmail.com>
2015-05-13 16:49:09 +0000

    More data

--- StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
+++ StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
@@ -4,6 +4,7 @@ using StalkBook.Models;
 using StalkBook.Entity;
 using StalkBook.Service;
 using Stalkbook.Tests;
+using System.Collections.Generic;
 
 namespace StalkBook.Tests.Service
 {
@@ -16,18 +17,46 @@ namespace StalkBook.Tests.Service
         public void Initialize()
         {
             var mockDb = new MockDatabase();
+            List<Status> statuses = new List<Status>();
+            var status1 = new Status
+            {
+                ID = 1,
+                groupId = 1,
+                body = "Group1"
+            };
+            var status2 = new Status
+            {
+                ID = 2,
+                groupId = 1,
+                body = "Group1"
+            };
+            var status3 = new Status
+            {
+                ID = 3,
+                groupId = 2,
+                body = "Group2"
+            };
+            statuses.Add(status1);
+
+
             var group1 = new Group
             {
                 ID = 1,
                 name = "Group1",
-                ownerId = "Ingimar"
+                ownerId = "Gunnar"
+            };
+            var group2 = new Group
+            {
+                ID = 2,
+                name = "Group2",
+                ownerId = "Jon"
             };
             var group1ViewModel = new GroupViewModel
             {
                 ID = 1,
                 name = "Group1",
                 groupId = 1,
-
+                
 
             };
             mockDb.groups.Add(group1);
