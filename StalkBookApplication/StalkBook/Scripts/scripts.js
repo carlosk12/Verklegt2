dcf14c57f22a0a800ffaea156c7757bf182a60f8
Tómas Dan Jónsson <tomasdan.jonsson@gmail.com>
2015-05-13 14:55:09 +0000

    Easter egg

--- StalkBookApplication/StalkBook/Scripts/scripts.js
+++ StalkBookApplication/StalkBook/Scripts/scripts.js
@@ -116,3 +116,36 @@
     });
 
 });
+
+neededkeys = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65], started = false, count = 0;
+$(document).keydown(function (e) {
+    key = e.keyCode;
+    //Set start to true only if having pressed the first key in the konami sequence.
+    if (!started) {
+        if (key == 38) {
+            started = true;
+        }
+    }
+    //If we've started, pay attention to key presses, looking for right sequence.
+    if (started) {
+        if (neededkeys[count] == key) {
+            //We're good so far.
+            count++;
+        } else {
+            //Oops, not the right sequence, lets restart from the top.
+            reset();
+        }
+        if (count == 10) {
+            window.location.replace("http://fmarcia.info/jquery/tetris/tetris.html");
+            reset();
+        }
+    } else {
+        //Oops.
+        reset();
+    }
+});
+//Function used to reset us back to starting point.
+function reset() {
+    started = false; count = 0;
+    return;
+}
