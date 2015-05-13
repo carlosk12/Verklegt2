841ebca8955a346887dbb0292a2f784d33af37ec
ingimar9 <ingimar9@gmail.com>
2015-05-13 22:51:45 +0000

    GroupService Unit Tests

--- StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
+++ StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
@@ -5,6 +5,7 @@ using StalkBook.Entity;
 using StalkBook.Service;
 using Stalkbook.Tests;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StalkBook.Tests.Service
 {
@@ -12,70 +13,188 @@ namespace StalkBook.Tests.Service
     public class GroupServiceTest
     {
         private GroupService service;
+		private MockDatabase mockDb = new MockDatabase();
 
         [TestInitialize]
         public void Initialize()
-        {
-            var mockDb = new MockDatabase();
-            List<Status> statuses = new List<Status>();
-            var status1 = new Status
-            {
-                ID = 1,
-                groupId = 1,
-                body = "Group1"
-            };
-            var status2 = new Status
-            {
-                ID = 2,
-                groupId = 1,
-                body = "Group1"
-            };
-            var status3 = new Status
-            {
-                ID = 3,
-                groupId = 2,
-                body = "Group2"
-            };
-            statuses.Add(status1);
+        {           
+			var status1 = new Status { ID = 1, groupId = 1, body = "status1", userId = "User1" };
+			var status2 = new Status { ID = 2, groupId = 1, body = "status2", userId = "User1" };
+			var status3 = new Status { ID = 3, groupId = 2, body = "status3", userId = "User2" };
 
+            List<Status> statuses = new List<Status>{status1, status2, status3};
+			var group1 = new Group { ID = 1, name = "group1", ownerId = "User1" };
+			var group2 = new Group { ID = 2, name = "group2", ownerId = "User2" };
+			var group3 = new Group { ID = 2, name = "group2", ownerId = "User2" };
 
-            var group1 = new Group
-            {
-                ID = 1,
-                name = "Group1",
-                ownerId = "Gunnar"
-            };
-            var group2 = new Group
-            {
-                ID = 2,
-                name = "Group2",
-                ownerId = "Jon"
-            };
-            var group1ViewModel = new GroupViewModel
-            {
-                ID = 1,
-                name = "Group1",
-                groupId = 1,
-                
-
-            };
-            mockDb.groups.Add(group1);
-
+			var groupProfileFK1 = new GroupProfileFK { ID = 1, groupID = 1, profileID = "User1" };
+			var groupProfileFK2 = new GroupProfileFK { ID = 2, groupID = 2, profileID = "User1" };
+			var userRating = new UserStatusRating { ID = 1, rating = 2, statusId = 1, userId = "User1" };
+ 			
+			foreach(var item in statuses)
+			{
+				mockDb.userStatuses.Add(item);
+			}
+			mockDb.groups.Add(group1);
+			mockDb.groups.Add(group2);
+			mockDb.groups.Add(group3);
+			mockDb.groupProfileFks.Add(groupProfileFK1);
+			mockDb.groupProfileFks.Add(groupProfileFK2);
+			mockDb.userStatusRating.Add(userRating);
 
             service = new GroupService(mockDb);
         }
 
+		[TestMethod]
+		public void RemoveUserFromGroup()
+		{
+			// Arrange
+			var numberOfGroupProfileFK = 1;
+
+			// Act
+			service.RemoveUserFromGroup("User1", 2);
+
+			// Assert
+			Assert.AreEqual(numberOfGroupProfileFK, mockDb.groupProfileFks.Count());
+			Assert.IsTrue(mockDb.groupProfileFks.ElementAt(0).groupID == 1);
+		}
+
+		[TestMethod]
+		public void AddUserToGroup()
+		{
+			// Arrange
+			var numberOfGroupProfileFK = 3;
+			var groupProfileFK3 = new GroupProfileFK {ID = 0, groupID = 1, profileID = "User3" };
+
+			// Act
+			service.AddUserToGroup("User3", 1);
+
+			// Assert
+			Assert.AreEqual(numberOfGroupProfileFK, mockDb.groupProfileFks.Count());
+			Assert.AreEqual(mockDb.groupProfileFks.ElementAt(2).profileID, groupProfileFK3.profileID);
+		}
+
+		[TestMethod]
+		public void GetAllGroups()
+		{
+			// Arrange
+			var numberOfGroups = 3;
+
+			// Act
+			var result = service.GetAllGroups();
+
+			// Assert
+			Assert.AreEqual(numberOfGroups, result.Count());
+		}
+
+		[TestMethod]
+		public void GetGroupsByUserId()
+		{
+			// Arrange
+			var group1 = new Group { ID = 1, name = "group1", ownerId = "User1" };
+			var group2 = new Group { ID = 2, name = "group2", ownerId = "User2" };
+
+			// Act
+			var result = service.GetGroupsByUserId("User1");
+
+			// Assert
+			Assert.AreEqual(group1.name, result.ElementAt(0).name);
+			Assert.AreEqual(group2.name, result.ElementAt(1).name);
+		}
+		[TestMethod]
+		public void GetGroupsByUserIdUserDontExist()
+		{
+			// Arrange
+			var numberOfGroups = 0;
+
+			// Act
+			var result = service.GetGroupsByUserId("User3");
+
+			// Assert
+			Assert.AreEqual(numberOfGroups, result.Count());
+		}
+
+		[TestMethod]
+		public void CreateGroup()
+		{
+			// Arrange
+			var numberOfGroups = 4;
+			var group3 = new Group { ID = 3, name = "group3", ownerId = "User3" };
+
+			// Act
+			service.CreateGroup("Gunnar",group3);
+
+			// Assert
+			Assert.AreEqual(numberOfGroups, mockDb.groups.Count());
+		}
+
+		[TestMethod]
+		public void DeleteGroup()
+		{
+			// Arrange
+			var numberOfGroups = 2;
+			var numberOfGroupProfileFK = 1;
+			var numberOfStatuses = 1;
+			var numberOfStatusRatings = 0;
+
+			// Act
+			service.DeleteGroup(1);
+
+			// Assert
+			Assert.AreEqual(numberOfGroups, mockDb.groups.Count());
+			Assert.AreEqual(numberOfGroupProfileFK, mockDb.groupProfileFks.Count());
+			Assert.AreEqual(numberOfStatuses, mockDb.userStatuses.Count());
+			Assert.AreEqual(numberOfStatusRatings, mockDb.userStatusRating.Count());
+		}
+
         [TestMethod]
-        public void GetGroupsByUserId()
+        public void GetGroupsByIdGroupInDatabase()
         {
             // Arrange
-            //HomeController controller = new HomeController();
+			var status1 = new Status { ID = 1, groupId = 1, body = "status1", userId = "User1" };
+			var status2 = new Status { ID = 2, groupId = 1, body = "status2", userId = "User1" };
+			List<Status> statuses = new List<Status> { status1, status2};
+			var group1 = new Group { ID = 1, name = "group1", ownerId = "User1" };
+			var expectedResult = new GroupViewModel { name = "group1", groupStatuses = statuses, groupId = 1 };
 
             // Act
-            //ViewResult result = controller.Index() as ViewResult;
+			var result = service.GetGroupById(1);
 
             // Assert
-            Assert.IsNotNull(null);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(0).ID, result.groupStatuses.ElementAt(0).ID);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(1).ID, result.groupStatuses.ElementAt(1).ID);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(0).groupId, result.groupStatuses.ElementAt(0).groupId);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(1).groupId, result.groupStatuses.ElementAt(1).groupId);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(0).body,result.groupStatuses.ElementAt(0).body);
+			Assert.AreEqual(expectedResult.groupStatuses.ElementAt(1).body, result.groupStatuses.ElementAt(1).body);
+			Assert.AreEqual(expectedResult.name, result.name);
+			Assert.AreEqual(expectedResult.groupId, result.groupId);				
         }
+
+		[TestMethod]
+		public void GetGroupsByIGroupNotInDatabase()
+		{
+			// Arrange
+			
+			// Act
+			var result = service.GetGroupById(3);
+
+			// Assert
+			Assert.IsNull(result);
+		}
+
+		[TestMethod]
+		public void PostStatus()
+		{
+			// Arrange
+			var numberOfStatus = 4;
+			var status4 = new Status { ID = 4, groupId = 1, body = "status4", userId = "User4" };
+
+			// Act
+			service.PostStatus(status4.userId, status4);
+
+			// Assert
+			Assert.AreEqual(numberOfStatus, mockDb.userStatuses.Count());
+		}
     }
 }
--- StalkBookApplication/StalkBook/Service/GroupService.cs
+++ StalkBookApplication/StalkBook/Service/GroupService.cs
@@ -20,120 +20,190 @@ namespace StalkBook.Service
 
         public void CreateGroup(string userId, Group groupName)
         {
-            groupName.ownerId = userId;
-            groupName.timeCreated = System.DateTime.Now;
+			try
+			{
+				groupName.ownerId = userId;
+				groupName.timeCreated = System.DateTime.Now;
 
-            db.groups.Add(groupName);
-            db.SaveChanges();
+				db.groups.Add(groupName);
+				db.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+			}       
         }
 
         public GroupViewModel GetGroupById(int groupId)
         {
-            var model = new GroupViewModel();
-            var group = (from g in db.groups
-                         where g.ID == groupId
-                         select g).SingleOrDefault();
-            model.creationDate = group.timeCreated;
-            model.name = group.name;
-            model.groupStatuses = (from gs in db.userStatuses
-                                   where gs.groupId == groupId
-								   orderby gs.timeCreated descending
-                                   select gs).ToList();
-            model.groupId = groupId;
-            
-            return model;
+			try
+			{
+				var model = new GroupViewModel();
+				var group = (from g in db.groups
+							 where g.ID == groupId
+							 select g).SingleOrDefault();
+				model.creationDate = group.timeCreated;
+				model.name = group.name;
+				model.groupStatuses = (from gs in db.userStatuses
+									   where gs.groupId == groupId
+									   orderby gs.timeCreated descending
+									   select gs).ToList();
+				model.groupId = groupId;
+
+				return model;
+			}
+			catch(Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+				return null;
+			}          
         }
 
         public void DeleteGroup(int groupId)
-        {           
-                var deleteFK = (from s in db.groupProfileFks
-                                where s.groupID == groupId                            
-                                select s).ToList();             
-                var deleteGroupStatuses = (from s in db.userStatuses
-                                           where s.groupId == groupId
-                                           select s).ToList();
-                List<int> statusIds = (from s in db.userStatuses
-                                 where s.groupId == groupId
-                                 select s.ID).ToList();                
-                var deleteStatusRating = (from r in db.userStatusRating where statusIds.Contains(r.statusId)  select r).ToList();
-                var deleteGroup = (from g in db.groups
-                                   where g.ID == groupId
-                                   select g).FirstOrDefault(); 
-     
-                foreach(var item in deleteFK)
-                {
-                    db.groupProfileFks.Remove(item); 
-                }
-                foreach (var item in deleteGroupStatuses)
-                {
-                    db.userStatuses.Remove(item);
-                }
-                foreach (var item in deleteStatusRating)
-                {
-                    db.userStatusRating.Remove(item);
-                }
-                         
-                db.groups.Remove(deleteGroup);
-          
+        {
+           	try
+			{
+				var deleteFK = (from s in db.groupProfileFks
+								where s.groupID == groupId
+								select s).ToList();
+				var deleteGroupStatuses = (from s in db.userStatuses
+										   where s.groupId == groupId
+										   select s).ToList();
+				List<int> statusIds = (from s in db.userStatuses
+									   where s.groupId == groupId
+									   select s.ID).ToList();
+				var deleteStatusRating = (from r in db.userStatusRating where statusIds.Contains(r.statusId) select r).ToList();
+				var deleteGroup = (from g in db.groups
+								   where g.ID == groupId
+								   select g).FirstOrDefault();
 
-                db.SaveChanges();
+				foreach (var item in deleteFK)
+				{
+					db.groupProfileFks.Remove(item);
+				}
+				foreach (var item in deleteGroupStatuses)
+				{
+					db.userStatuses.Remove(item);
+				}
+				foreach (var item in deleteStatusRating)
+				{
+					db.userStatusRating.Remove(item);
+				}
+				db.groups.Remove(deleteGroup);
+				db.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);			
+			}        
         }
 
         public void PostStatus(string userId, Status status)
         {
-            status.userId = userId;
-            status.timeCreated = System.DateTime.Now;
-            status.fullName = (from p in db.profiles
-                                   where p.userID == userId
-                                   select p.name).SingleOrDefault();
-            if (!String.IsNullOrEmpty(status.urlToPic))
-            {
-                status.urlToPic = status.urlToPic;
-            }
-            db.userStatuses.Add(status);       
-            db.SaveChanges();
+			try
+			{
+				status.userId = userId;
+				status.timeCreated = System.DateTime.Now;
+				status.fullName = (from p in db.profiles
+								   where p.userID == userId
+								   select p.name).SingleOrDefault();
+				if (!String.IsNullOrEmpty(status.urlToPic))
+				{
+					status.urlToPic = status.urlToPic;
+				}
+				db.userStatuses.Add(status);
+				db.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+			}     
         }
 
         public IEnumerable<Group> GetGroupsByUserId(string userId)
         {
-            List<string> result1 = (from s in db.groupProfileFks
-                where s.profileID == userId
-                select s.groupID.ToString()).ToList();
-
-            var result = (from gr in db.groups where result1.Contains(gr.ID.ToString()) select gr).ToList();
-
-            return result;
+			try
+			{
+				List<string> result1 = (from s in db.groupProfileFks
+										where s.profileID == userId
+										select s.groupID.ToString()).ToList();
+				var result = (from gr in db.groups where result1.Contains(gr.ID.ToString()) select gr).ToList();
+				return result;
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+				return null;
+			}                     
         }
 
         public IEnumerable<Group> GetAllGroups()
         {
-            List<Group> result = (from s in db.groups
-                select s).ToList();
+			try
+			{
+				List<Group> result = (from s in db.groups
+									  select s).ToList();
 
-            return result;
+				return result;
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+				return null;
+			}        
         }
 
         public void AddUserToGroup(string userId, int groupId)
         {
-            var group = new GroupProfileFK();
-            group.groupID = groupId;
-            group.profileID = userId;
-            db.groupProfileFks.Add(group);
-            db.SaveChanges();
+			try
+			{
+				var group = new GroupProfileFK();
+				group.groupID = groupId;
+				group.profileID = userId;
+				db.groupProfileFks.Add(group);
+				db.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+			}            
         }
 
         public void RemoveUserFromGroup(string userId, int groupId)
         {
-            var removeuser = (from s in db.groupProfileFks
-                            where s.profileID == userId
-                            where s.groupID == groupId
-                            select s).FirstOrDefault();
+			try
+			{
+				var removeuser = (from s in db.groupProfileFks
+								  where s.profileID == userId
+								  where s.groupID == groupId
+								  select s).FirstOrDefault();
 
-            if (!String.IsNullOrEmpty(userId) && removeuser != null)
-            {
-                db.groupProfileFks.Remove(removeuser);
-                db.SaveChanges();
-            }
+				if (!String.IsNullOrEmpty(userId) && removeuser != null)
+				{
+					db.groupProfileFks.Remove(removeuser);
+					db.SaveChanges();
+				}
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.Write(ex.Message + Environment.NewLine + "Location of error: " + ex.Source +
+					Environment.NewLine + "StackTrace: " + Environment.NewLine + ex.StackTrace +
+					Environment.NewLine + "Time : " + DateTime.Now + Environment.NewLine);
+			}            
         }
     }
 }
