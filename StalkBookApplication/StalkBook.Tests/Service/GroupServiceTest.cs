f659595c44fa5ddf696db6929c55c68c330c745c
Ingimar Örn Oddsson <ingimar9@gmail.com>
2015-05-13 14:11:17 +0000

    GroupServiceTest

---
+++ StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StalkBook.Tests.Service
+{
+    [TestClass]
+    public class GroupServiceTest
+    {
+        [TestMethod]
+        public void GetGroupsByUserId()
+        {
+            // Arrange
+            //HomeController controller = new HomeController();
+
+            // Act
+            //ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            //Assert.IsNotNull(result);
+        }
+    }
+}
