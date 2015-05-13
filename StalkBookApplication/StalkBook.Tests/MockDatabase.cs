88b04933e9a27b6a455dc616a1f079de4afeec29
Ingimar Örn Oddsson <ingimar9@gmail.com>
2015-05-13 15:54:46 +0000

    Undirbúningur fyrir unit test

---
+++ StalkBookApplication/StalkBook.Tests/MockDatabase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FakeDbSet;
+using System.Data.Entity;
+using StalkBook.Models;
+using StalkBook.Entity;
+
+namespace Stalkbook.Tests
+{
+    /// <summary>
+    /// This is an example of how we'd create a fake database by implementing the 
+    /// same interface that the BookeStoreEntities class implements.
+    /// </summary>
+    public class MockDatabase : IAppDataContext
+    {
+        /// <summary>
+        /// Sets up the fake database.
+        /// </summary>
+        public MockDatabase()
+        {
+            // We're setting our DbSets to be InMemoryDbSets rather than using SQL Server.
+            this.userStatuses = new InMemoryDbSet<Status>();
+            this.groups = new InMemoryDbSet<Group>();
+            this.profiles = new InMemoryDbSet<Profile>();
+            this.groupProfileFks = new InMemoryDbSet<GroupProfileFK>();
+            this.stalking = new InMemoryDbSet<Stalking>();
+            this.userStatusRating = new InMemoryDbSet<UserStatusRating>();
+        }
+
+        public IDbSet<Status> userStatuses { get; set; }
+        public IDbSet<Group> groups { get; set; }
+        public IDbSet<Profile> profiles { get; set; }
+        public IDbSet<GroupProfileFK> groupProfileFks { get; set; }
+        public IDbSet<Stalking> stalking { get; set; }
+        public IDbSet<UserStatusRating> userStatusRating { get; set; }
+
+        public int SaveChanges()
+        {
+            // Pretend that each entity gets a database id when we hit save.
+            int changes = 0;
+            //changes += DbSetHelper.IncrementPrimaryKey<Author>(x => x.AuthorId, this.Authors);
+            //changes += DbSetHelper.IncrementPrimaryKey<Book>(x => x.BookId, this.Books);
+
+            return changes;
+        }
+
+        public void Dispose()
+        {
+            // Do nothing!
+        }
+    }
+}
--- StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
+++ StalkBookApplication/StalkBook.Tests/Service/GroupServiceTest.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StalkBook.Service;
+using Stalkbook.Tests;
 
 namespace StalkBook.Tests.Service
 {
     [TestClass]
     public class GroupServiceTest
     {
+        private GroupService service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var mockDb = new MockDatabase();
+            service = new GroupService(mockDb);
+        }
+
         [TestMethod]
         public void GetGroupsByUserId()
         {
