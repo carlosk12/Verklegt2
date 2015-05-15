1150cf057b0fef694599b1bd5599ad71528785a3
Ingimar Örn Oddsson <ingimar9@gmail.com>
2015-05-15 16:32:02 +0000

    Comment ProfileService

--- StalkBookApplication/StalkBook/Service/ProfileService.cs
+++ StalkBookApplication/StalkBook/Service/ProfileService.cs
@@ -17,10 +17,11 @@ namespace StalkBook.Service
 
 		public ProfileViewModel GetProfile(string id)
 		{
+            // Get profile of the user with id.
 			var profileInfo = (from p in db.profiles
 						where p.userID == id
 						select p).FirstOrDefault();
-
+            // Get all the statuses of the user that were not posted in a group.
             List<Status> myStatuses = (from s in db.userStatuses
                               where s.userId == id
                               where s.groupId == null
@@ -34,6 +35,7 @@ namespace StalkBook.Service
             result.userStatuses = myStatuses;
             result.userID = profileInfo.userID;
 			result.profilePicUrl = profileInfo.profilePicUrl;
+            // Id of all users that the userId is stalking. Including himself.
 			result.stalking = (	from s in db.stalking
 								where s.userId == id
 								select s.stalkedId).ToList();
@@ -67,14 +69,16 @@ namespace StalkBook.Service
 
 		public void DeleteStatus(int statusId)
 		{
+            // Get status with statusId
 			var deleteStatus = (from s in db.userStatuses
 								where s.ID == statusId
 								select s).SingleOrDefault();
-
+            // Get all the ratings of that status.
 			var deleteStatusRatings = (from s in db.userStatusRating
 									   where s.statusId == statusId
 									   select s).ToList();
 
+            // Then remove it all from the database and save.
 			db.userStatuses.Remove(deleteStatus);
             foreach(var item in deleteStatusRatings)
             {
@@ -87,22 +91,26 @@ namespace StalkBook.Service
 		public IEnumerable<ProfileViewModel> ViewStalking(string Id, string theUserId)
 		{
 			List<ProfileViewModel> profiles = new List<ProfileViewModel>();
-
+            // Get all the users that the user with Id is stalking.
 			var stalkList = (from p in db.stalking
 						  where p.userId == Id
 						  select p).ToList();
-
+            // Get the profile of the user with Id.
 			ProfileViewModel myProfile = this.GetProfile(Id);
 
+            // Add all the profiles that the user with Id is stalking.
 			foreach (var item in stalkList)
 			{
 				profiles.Add( GetProfile(item.stalkedId) );
 			}
 
+            // Order the profiles in alphabetic order.
 			profiles = profiles.OrderBy(x => x.name).ToList();
+            // Get the id of all the users that the user that is logged in is stalking.
             myProfile.myStalkings = (from p in db.stalking
                                      where p.userId == theUserId
                                      select p.stalkedId).ToList();
+            // Insert the profile of the user with id at the front of the list.
 			profiles.Insert(0, myProfile);
 
 			return profiles;
@@ -111,22 +119,24 @@ namespace StalkBook.Service
 		public IEnumerable<ProfileViewModel> ViewStalkers(string Id, string theUserId)
 		{
 			List<ProfileViewModel> profiles = new List<ProfileViewModel>();
-
+            // Get all the users that are stalking the user with id.
 			var stalkList = (from p in db.stalking
 							 where p.stalkedId == Id
 							 select p).ToList();
-
+            // Get the profile of the user with Id.
 			ProfileViewModel myProfile = this.GetProfile(Id);
-
+            // Add all the profiles that are stalking the user with id.
 			foreach (var item in stalkList)
 			{
 				profiles.Add(GetProfile(item.userId));
 			}
-
+            // Order the profiles in alphabetic order.
 			profiles = profiles.OrderBy(x => x.name).ToList();
+            // Get the id of all the users that the user that is logged in is stalking.
             myProfile.myStalkings = (from p in db.stalking
                                      where p.userId == theUserId
                                      select p.stalkedId).ToList();
+            // Insert the profile of the user with id at the front of the list.
 			profiles.Insert(0, myProfile);
 
 			return profiles;
